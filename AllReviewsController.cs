9d2cb3b1c7c30f2f8ed7a0a72824da28178a0275
mathew perrow <mathew.perrow@gmail.com>
2017-03-17 17:09:13 -0700

    Added new controller  and partial views for reviews
    Item module now has reviews functionality

---
+++ AllReviewsController.cs
@@ -0,0 +1,47 @@
+﻿using Blue_Ribbon.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Blue_Ribbon.DAL;
+
+namespace Blue_Ribbon.Controllers
+{
+    public class AllReviewsController : Controller
+    {
+        private BRContext db = new BRContext();
+        // GET: AllReviews
+        public ActionResult Index(string id)
+        {
+
+            // Making a list of the reviews associated with the product
+            // Pulling all with the given ID
+
+
+            List<ReviewLog> Reviews = (from log in db.ReviewLog
+                                       where log.ASIN.Equals(id) && log.CustomerReviewed == true
+                                       select log).ToList();
+
+
+            return View(Reviews);
+
+        }
+
+        public new ActionResult PartialView(string id)
+        {
+            // Making a list of the reviews associated with the product
+            // Pulling all with the given ID
+
+
+            List<ReviewLog> Reviews = (from log in db.ReviewLog
+                                       where log.ASIN.Equals(id) && log.CustomerReviewed == true
+                                       select log).ToList();
+
+            // Returning a partial view to show in the product Model
+            return PartialView(Reviews);
+
+        }
+    }
+
+}
